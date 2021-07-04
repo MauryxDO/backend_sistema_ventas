7b07863d0b5a3cb5bb8dbd7e8f7533b464e53ea1
Mauricio <mau03041@gmail.com>
2021-07-04 17:34:23 -0500

    Agregando función ingreso de articulos

--- models/index.js
+++ models/index.js
@@ -1,9 +1,11 @@
 import Categoria from './categoria';
 import Articulo from './articulo';
 import Usuario from './usuario';
+import Ingreso from './ingreso';
 
 export default {
     Categoria,
     Articulo,
-    Usuario
+    Usuario,
+    Ingreso
 }
