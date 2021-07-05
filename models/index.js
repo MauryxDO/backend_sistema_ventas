9a2fb588b76e05b870036d54ed627ff952626e01
naye_ramos <nayeocampo2108@gmail.com>
2021-07-04 23:18:50 -0500

    configuracion del modulo venta

--- models/index.js
+++ models/index.js
@@ -2,10 +2,15 @@ import Categoria from './categoria';
 import Articulo from './articulo';
 import Usuario from './usuario';
 import Ingreso from './ingreso';
+import Persona from '/persona';
+import Venta from '/venta';
+
 
 export default {
     Categoria,
     Articulo,
     Usuario,
-    Ingreso
+    Ingreso,
+    Persona,
+    Venta
 }
