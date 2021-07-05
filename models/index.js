91c196a4a51c5fb781f97bceda5d291086c229f2
Mauricio <mau03041@gmail.com>
2021-07-05 02:49:05 -0500

    Correción a las importaciones en index.js

--- models/index.js
+++ models/index.js
@@ -2,8 +2,8 @@ import Categoria from './categoria';
 import Articulo from './articulo';
 import Usuario from './usuario';
 import Ingreso from './ingreso';
-import Persona from '/persona';
-import Venta from '/venta';
+import Persona from './persona';
+import Venta from './venta';
 
 
 export default {
